e9c7997fb49d9835bf33fd2ba82fb22c3e7b6d44
JoakinAD <joaquin.abad.diaz@gmail.com>
2026-05-12 12:04:04 +0200

    Chapter 1.5

--- cout_example.cpp
+++ cout_example.cpp
@@ -5,15 +5,21 @@ int main()
 	int width; // define a variable named width
 	width = 5; // copy assignment of value 5 into variable width
 
-	std::cout << width; // prints 5
-
-    std::cout << "\n"; 
+	std::cout << width << std::endl; // prints 5
 
 	width = 7; // change value stored in variable width to 7
 
 	std::cout << width; // prints 7
 
     std::cout << "\n";
+
+    std::cout << width << " + " << width << " = 14\n";
+
+    std::cout << "Enter a number: ";
+
+    std::cin >> width;
+
+    std::cout << "Your number is: " << width << "\n";
 
 	return 0;
 }
